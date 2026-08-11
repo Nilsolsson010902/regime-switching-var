33d0320cb72a3166871a623703b1636175b1d10d
Nilsolsson010902 <nilsjohn6@gmail.com>
2026-08-11 11:08:46 +0200

    more forward algorithm tests added

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -1,27 +1,36 @@
 import numpy as np
 from src.markov_model.forward import forward_filtering
+import pytest
 
-def test_forward_filtering():
+
+@pytest.fixture
+def  initial_probability():
+    return np.array([0.5, 0.5])
+
+@pytest.fixture
+def transition_matrix():
+    return np.array([
+        [0.95, 0.05],
+        [0.20, 0.80]
+    ])
+
+@pytest.fixture
+def log_emission_matrix():
+    return np.log(np.array([
+        [0.90, 0.10],
+        [0.20, 0.80],
+        [0.10, 0.90]
+    ]))
+
+def test_forward_filtering(initial_probability, transition_matrix, log_emission_matrix):
     """
     
     Test the forward filtering algorithm with a simple example.
 
     """
-    initial_probabilities = np.array([0.5, 0.5])
 
-    transition_matrix = np.array([
-        [0.95, 0.05],
-        [0.20, 0.80]
-    ])
 
-    emission_matrix = np.array([
-        [0.90, 0.10],
-        [0.20, 0.80],
-        [0.10, 0.90]
-    ])
-
-    log_emissions = np.log(emission_matrix)
-    log_alpha = forward_filtering(initial_probabilities, transition_matrix, log_emissions)
+    log_alpha = forward_filtering(initial_probability, transition_matrix, log_emission_matrix)
     alpha = np.exp(log_alpha)
 
     expected = np.array([
@@ -31,3 +40,47 @@ def test_forward_filtering():
     ])
 
     assert np.allclose(alpha, expected, atol=1e-3)
+
+def test_forward_probabilities_sum_to_one(initial_probability, transition_matrix, log_emission_matrix):
+    """
+    Test that the forward probabilities sum to 1 at each time step.
+    """
+
+    log_alpha = forward_filtering(
+        initial_probability,
+        transition_matrix,
+        log_emission_matrix
+    )
+
+    alpha = np.exp(log_alpha)
+
+    assert np.allclose(
+        alpha.sum(axis=1),
+        1.0
+    )
+
+def test_forward_output_shape(initial_probability, transition_matrix,log_emission_matrix):
+    """
+    Test that the output of the forward filtering algorithm has the correct shape.
+    """
+    log_alpha = forward_filtering(
+        initial_probability,
+        transition_matrix,
+        log_emission_matrix
+    )
+
+    assert log_alpha.shape == (3, 2)
+
+
+
+def test_forward_values_are_finite(initial_probability, transition_matrix, log_emission_matrix):
+    """
+    Test that the forward probabilities are finite.
+    """
+    log_alpha = forward_filtering(
+        initial_probability,
+        transition_matrix,
+        log_emission_matrix
+    )
+
+    assert np.all(np.isfinite(log_alpha))
